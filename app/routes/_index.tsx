2aa6cf1ff15fbebd16777a1ba8530450668d684f
coder7475 <robiulhossain7475@gmail.com>
2024-12-01 09:56:00 +0600

    feat: create Navbar for routing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div>
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold">Remix Template</h1>
---
+++ app/routes/about.tsx
@@ -0,0 +1,16 @@
+// export async function loader() {
+//   // provides data to the component
+// }
+
+export default function About() {
+  // renders the UI
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center">About Us</h1>
+    </div>
+  );
+}
+
+// export async function action() {
+//   // updates persistent data
+// }
